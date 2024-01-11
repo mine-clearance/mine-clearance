78976a149fb65768cd5444b2f70f195b7ee634c4
Guillaume Dorschner <guillaume.dorschner@icloud.com>
2024-01-11 23:10:07 +0100

    create the popup

---
+++ Software/src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Popup from './lib/Popup.svelte';
+  import "./app.css";
+</script>
+
+<main class="container">
+  <h1>Welcome to Tauri!</h1>
+
+  <div class="row">
+    <a href="https://svelte.dev" target="_blank">
+      <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+
+  <Popup show={true} title={"Danger"} message={"Altitude basse veuillez reprendre le contrôle"}/>
+
+</main>
+
+<style>
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00);
+  }
+</style>
---
+++ Software/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        'blue': '#020C31',
+        'red': '#FF6363'
+      },
+    }
+  },
+  plugins: []
+};
